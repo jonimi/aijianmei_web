e7755ebdbb8317660536e0b3bd4b5e3490dc305e
Tom Callon <tomcallon2010@gmail.com>
2013-02-28 13:02:02 +0800

    commited new things man .

--- htdocs/htdocs/_runtime/~temp/0/8/~@088134fa23d0870d92f55fb2c70ba35b.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000000a:9:{s:8:"__hash__";s:32:"3d1ba92bca03cacb92bb32379f880356";s:16:"attach_path_rule";s:7:"Y/md/H/";s:15:"attach_max_size";d:2;s:22:"attach_allow_extension";s:59:"jpg,gif,png,jpeg,bmp,zip,rar,doc,xls,ppt,docx,xlsx,pptx,pdf";s:23:"attach_watermark_closed";s:1:"0";s:25:"attach_watermark_minwidth";s:3:"200";s:26:"attach_watermark_minheight";s:3:"200";s:20:"attach_watermark_txt";s:8:"@{uname}";s:25:"attach_watermark_fontsize";s:2:"12";}
-?>
